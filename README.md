0bb7281589ccc8cf1045d891c0c3e7283b6fc97c
Zeke Gabrielse <ezekg@yahoo.com>
2022-07-28 09:37:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Example Python Cryptographic Machine Files
+
+This is an example of how to verify and decrypt [cryptographic machine files](https://keygen.sh/docs/api/cryptography/#cryptographic-lic)
+in Python, using Ed25519 signing and AES-256-GCM encryption. This example
+verifies the `aes-256-gcm+ed25519` algorithm.
+
+## Running the example
+
+First up, add an environment variable containing your Ed25519 public key:
+```bash
+export KEYGEN_PUBLIC_KEY='799efc7752286e6c3815b13358d98fc0f0b566764458adcb48f1be2c10a55906'
+```
+
+You can either run each line above within your terminal session before
+starting the app, or you can add the above contents to your `~/.bashrc`
+file and then run `source ~/.bashrc` after saving the file.
+
+Next, install dependencies with [`pip`](https://packaging.python.org/):
+
+```
+pip install -r requirements.txt
+```
+
+Then run the script, passing in a `path` to a machine file, and a `license`
+key as arguments:
+
+```bash
+python main.py --license 'A_LICENSE_KEY' \
+  --path /etc/keygen/machine.lic
+```
+
+Or run one of the pre-defined examples:
+
+```bash
+KEYGEN_PUBLIC_KEY='e8601e48b69383ba520245fd07971e983d06d22c4257cfd82304601479cee788' python main.py \
+  --fingerprint '198e9fe586114844f6a4eaca5069b41a7ed43fb5a2df84892b69826d64573e39' \
+  --license 'B10760-1B177D-656D1F-C03298-9AF89E-V3' \
+  --path examples/machine.lic
+```
+
+The following will happen:
+
+1. The current device will be fingerprinted, using a SHA-256 digest of the
+   device's MAC address. You may want to look into alternative fingerprinting,
+   depending on your expected environment.
+1. The machine file's authenticity will be verified using Ed25519, by verifing
+   its signature using the public key.
+1. The machine file will be decrypted using the license key and fingerprint
+   as the decryption key.
+
+If everything checks out, the script will print the decrypted contents of
+the machine file — the machine object, with any included data. If it
+fails, check your inputs.
+
+## Questions?
+
+Reach out at [support@keygen.sh](mailto:support@keygen.sh) if you have any
+questions or concerns!
---
+++ main.py
@@ -0,0 +1,108 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidKey, InvalidTag
+from ed25519 import BadSignatureError
+from uuid import getnode as get_mac
+import argparse
+import ed25519
+import base64
+import json
+import sys
+import os
+
+def fingerprint():
+  """
+  fingerprint returns a hex-encoded SHA-256 digest of the current MAC address.
+  """
+  mac    = str(get_mac())
+  digest = hashes.Hash(hashes.SHA256())
+  digest.update(mac.encode())
+  return digest.finalize().hex()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-p', '--path',        dest='path',        required=True,         help='Path to machine file (required)')
+parser.add_argument('-l', '--license',     dest='license',     required=True,         help='License key (required)')
+parser.add_argument('-f', '--fingerprint', dest='fingerprint', default=fingerprint(), help='Machine fingerprint')
+
+args = parser.parse_args()
+
+# Read the machine file
+machine_file = None
+
+try:
+  with open(args.path) as f:
+    machine_file = f.read()
+except (FileNotFoundError, PermissionError):
+  print('[error] path does not exist! (or permission was denied)')
+
+  sys.exit(1)
+
+# Strip the header and footer from the machine file certificate
+payload = machine_file.lstrip('-----BEGIN MACHINE FILE-----\n') \
+                      .rstrip('-----END MACHINE FILE-----\n')
+
+# Decode the payload and parse the JSON object
+data = json.loads(base64.b64decode(payload))
+
+# Retrieve the enc and sig properties
+enc = data['enc']
+sig = data['sig']
+alg = data['alg']
+
+if alg != 'aes-256-gcm+ed25519':
+  print('[error] algorithm is not supported!')
+
+  sys.exit(1)
+
+# Verify using Ed25519
+try:
+  verify_key = ed25519.VerifyingKey(
+    os.environ['KEYGEN_PUBLIC_KEY'].encode(),
+    encoding='hex',
+  )
+
+  verify_key.verify(
+    base64.b64decode(sig),
+    ('machine/%s' % enc).encode(),
+  )
+except (AssertionError, BadSignatureError):
+  print('[error] verification failed!')
+
+  sys.exit(1)
+
+print('[info] verification successful!')
+
+# Hash the license key using SHA256
+digest = hashes.Hash(hashes.SHA256())
+digest.update(args.license.encode())
+digest.update(args.fingerprint.encode())
+key = digest.finalize()
+
+# Split and decode the enc value
+ciphertext, iv, tag = map(
+  lambda p: base64.b64decode(p),
+  enc.split('.'),
+)
+
+# Decrypt ciphertext
+try:
+  aes = Cipher(
+  algorithms.AES(key),
+  modes.GCM(iv, None, len(tag)),
+  default_backend(),
+  )
+  dec = aes.decryptor()
+
+  plaintext = dec.update(ciphertext) + \
+              dec.finalize_with_tag(tag)
+except (InvalidKey, InvalidTag):
+  print('[error] decryption failed!')
+
+  sys.exit(1)
+
+print('[info] decryption successful!')
+print(
+  json.dumps(json.loads(plaintext.decode()), indent=2)
+)
